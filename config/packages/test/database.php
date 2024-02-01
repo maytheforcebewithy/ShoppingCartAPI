f4ad99ad73ce81c8f1f07fdb6c2ec0e920f1cda8
Kira <kira.reitz@gmx.de>
2024-02-02 00:19:56 +0100

    Datenbank Anbindungen verbessert

--- config/packages/test/database.php
+++ config/packages/test/database.php
@@ -1,9 +1,9 @@
 <?php
 
 return [
-    'dsn' => 'pgsql:host=database;dbname=prod_db_name',
-    'username' => 'prod_db_user',
-    'password' => 'prod_db_password',
+    'dsn' => 'pgsql:host=test_database;dbname=test_db_name',
+    'username' => 'test_db_user',
+    'password' => 'test_db_password',
     'options' => [
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
         PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
