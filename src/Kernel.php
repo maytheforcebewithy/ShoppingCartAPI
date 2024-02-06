e4618c0131e933abf3ac5b6f1363277bab8aaef8
Kira <kira.reitz@gmx.de>
2024-02-06 19:30:09 +0100

    .

--- src/Kernel.php
+++ src/Kernel.php
@@ -3,9 +3,9 @@
 namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
-use Symfony\Component\HttpKernel\Kernel as BaseKernel;
-use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel implements CompilerPassInterface
 {
--- tests/ControllerTests/ProductControllerTest.php
+++ tests/ControllerTests/ProductControllerTest.php
@@ -3,7 +3,7 @@
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class ProductControllerTest extends WebTestCase
-{   
+{
     public function testGetAllProducts(): void
     {
         $client = static::createClient();
