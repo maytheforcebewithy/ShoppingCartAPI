cf9b86c709c103e2f1e7f9565486f86b49caef2f
Kira <kira.reitz@gmx.de>
2024-02-06 20:58:09 +0100

    fehlende datei hinzugeügt

---
+++ init.sql
@@ -0,0 +1,19 @@
+CREATE TABLE products (
+    id        SERIAL PRIMARY KEY,
+    name      TEXT,
+    price     DECIMAL,
+    quantity  INT
+);
+
+CREATE TABLE users {
+  id    SERIAL PRIMARY KEY,
+  name  TEXT, 
+  email TEXT
+}
+
+CREATE TABLE cart {
+  user_id     INT REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE,
+  product_id  INT REFERENCES products (id) ON UPDATE CASCADE
+  quantity    INT,
+  CONSTRAINT user_product_pkey PRIMARY KEY(user_id, product_id)
+}
