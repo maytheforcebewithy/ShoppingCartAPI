4e538ba0d48d305536c748fa0f46712140d2b0cc
Kira <kira.reitz@gmx.de>
2024-02-06 21:21:31 +0100

    kleine änderungen

--- init.sql
+++ init.sql
@@ -5,15 +5,15 @@ CREATE TABLE products (
     quantity  INT
 );
 
-CREATE TABLE users {
-  id    SERIAL PRIMARY KEY,
-  name  TEXT, 
-  email TEXT
-}
+CREATE TABLE users (
+    id    SERIAL PRIMARY KEY,
+    name  TEXT,
+    email TEXT
+);
 
-CREATE TABLE cart_items {
-  user_id     INT REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE,
-  product_id  INT REFERENCES products (id) ON UPDATE CASCADE
-  quantity    INT,
-  CONSTRAINT user_product_pkey PRIMARY KEY(user_id, product_id)
-}
+CREATE TABLE cart_items (
+    user_id     INT REFERENCES users (id) ON UPDATE CASCADE ON DELETE CASCADE,
+    product_id  INT REFERENCES products (id) ON UPDATE CASCADE,
+    quantity    INT,
+    CONSTRAINT user_product_pkey PRIMARY KEY(user_id, product_id)
+);
